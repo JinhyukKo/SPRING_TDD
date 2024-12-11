5d7e48a9d4aec5c05ade07116fe76a4ae39cef31
Jinhyuk <kobin1970@gmail.com>
2024-12-11 17:16:20 +0900

    user userdao

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import com.example.domain.User;
+import com.example.domain.UserDao;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        User user = new User();
+        UserDao userDao = new UserDao();
     }
 }
--- src/main/java/com/example/domain/UserDao.java
+++ src/main/java/com/example/domain/UserDao.java
@@ -1,4 +1,32 @@
 package com.example.domain;
 
+
+import java.sql.*;
+
 public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi","root","hyuk5907");
+        PreparedStatement ps = c.prepareStatement("insert into users(username,password) values(?,?)");
+        ps.setString(1,user.getUsername());
+        ps.setString(1,user.getPassword());
+        ps.executeUpdate();
+        ps.close();
+        c.close();
+    }
+    public User get(String username) throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi","root","hyuk5907");
+        PreparedStatement ps = c.prepareStatement("select * from users where username = ?");
+        ps.setString(1,username);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setUsername(rs.getString("username"));
+        user.setPassword(rs.getString("password"));
+        rs.close();
+        ps.close();
+        c.close();
+        return user;
+    }
 }
