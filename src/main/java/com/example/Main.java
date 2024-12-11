32661ef229b93f255f52f884810af98e4a90e1b2
Jinhyuk <kobin1970@gmail.com>
2024-12-11 17:29:32 +0900

    add main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,10 +3,19 @@ package com.example;
 import com.example.domain.User;
 import com.example.domain.UserDao;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws ClassNotFoundException, SQLException {
         System.out.println("Hello world!");
-        User user = new User();
+        User newUser = new User();
         UserDao userDao = new UserDao();
+        newUser.setPassword("password");
+        newUser.setUsername("username");
+        userDao.add(newUser);
+
+        User user = userDao.get(newUser.getUsername());
+        System.out.println(user.getUsername());
+        System.out.println(user.getPassword());
     }
 }
--- src/main/java/com/example/domain/UserDao.java
+++ src/main/java/com/example/domain/UserDao.java
@@ -5,18 +5,18 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi","root","hyuk5907");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi","root","password");
         PreparedStatement ps = c.prepareStatement("insert into users(username,password) values(?,?)");
         ps.setString(1,user.getUsername());
-        ps.setString(1,user.getPassword());
+        ps.setString(2,user.getPassword());
         ps.executeUpdate();
         ps.close();
         c.close();
     }
     public User get(String username) throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi","root","hyuk5907");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi","root","password");
         PreparedStatement ps = c.prepareStatement("select * from users where username = ?");
         ps.setString(1,username);
         ResultSet rs = ps.executeQuery();
