dd503352f0a275794a6c1489f9362307572a0237
Jinhyuk <kobin1970@gmail.com>
2024-12-14 19:50:58 +0900

    add count test

--- src/test/java/domain/UserDaoTest.java
+++ src/test/java/domain/UserDaoTest.java
@@ -13,29 +13,36 @@ import java.sql.SQLException;
 public class UserDaoTest {
     @Test
     public void addAndGet()  throws ClassNotFoundException, SQLException {
-
-
-        User newUser = new User();
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
         CountingConnectionCreatorDecorator connectionCreator = context.getBean("connectionCreator", CountingConnectionCreatorDecorator.class);
-        System.out.println(connectionCreator.getCount());
-
         userDao.deleteAll();
         assert userDao.getCount()==0 : "deleteAll failed";
-
+        User newUser = new User();
         newUser.setPassword("password");
         newUser.setUsername("username");
         userDao.add(newUser);
-
         User user = userDao.get(newUser.getUsername());
-
         assert userDao.getCount()==1 : "add failed";
         assert user.getUsername().equals(newUser.getUsername());
         assert user.getPassword().equals(newUser.getPassword());
+    }
+    @Test
+    public void count()  throws ClassNotFoundException, SQLException {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+        User user = new User("username1", "password1");
+        User user2 = new User("username2", "password2");
+        User user3 = new User("username3", "password3");
 
-
-
+        userDao.deleteAll();
+        assert userDao.getCount()==0 : "deleteAll failed";
+        userDao.add(user);
+        assert userDao.getCount()==1 : "add failed";
+        userDao.add(user2);
+        assert userDao.getCount()==2 : "add failed";
+        userDao.add(user3);
+        assert userDao.getCount()==3 : "add failed";
 
     }
 }
