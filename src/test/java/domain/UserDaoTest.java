70c3fa8c8a7d5845e64026490a49d724ee587b26
Jinhyuk <kobin1970@gmail.com>
2024-12-14 19:40:10 +0900

    remove dependency with DB

--- src/test/java/domain/UserDaoTest.java
+++ src/test/java/domain/UserDaoTest.java
@@ -13,23 +13,24 @@ import java.sql.SQLException;
 public class UserDaoTest {
     @Test
     public void addAndGet()  throws ClassNotFoundException, SQLException {
-        System.out.println(System.getProperty("java.class.path"));
-        User newUser = new User();
-//        UserDao userDao = new DaoFactory().getUserDao();
 
+
+        User newUser = new User();
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
         CountingConnectionCreatorDecorator connectionCreator = context.getBean("connectionCreator", CountingConnectionCreatorDecorator.class);
         System.out.println(connectionCreator.getCount());
+
+        userDao.deleteAll();
+        assert userDao.getCount()==0 : "deleteAll failed";
 
         newUser.setPassword("password");
         newUser.setUsername("username");
         userDao.add(newUser);
 
         User user = userDao.get(newUser.getUsername());
-        System.out.println(user.getUsername());
-        System.out.println(user.getPassword());
-        System.out.println(connectionCreator.getCount());
+
+        assert userDao.getCount()==1 : "add failed";
         assert user.getUsername().equals(newUser.getUsername());
         assert user.getPassword().equals(newUser.getPassword());
 
