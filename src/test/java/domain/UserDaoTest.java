a32c783bc231ff79e58aec9438db5ec40f38de2d
Jinhyuk <kobin1970@gmail.com>
2024-12-14 19:58:30 +0900

    enhance AddandGet test method

--- src/test/java/domain/UserDaoTest.java
+++ src/test/java/domain/UserDaoTest.java
@@ -13,19 +13,35 @@ import java.sql.SQLException;
 public class UserDaoTest {
     @Test
     public void addAndGet()  throws ClassNotFoundException, SQLException {
+        //Arragne
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
         CountingConnectionCreatorDecorator connectionCreator = context.getBean("connectionCreator", CountingConnectionCreatorDecorator.class);
+        User newUser1 = new User("username1","password1");
+        User newUser2 = new User("username2","password2");
+
+        //Assert
         userDao.deleteAll();
+
+        //Act
         assert userDao.getCount()==0 : "deleteAll failed";
-        User newUser = new User();
-        newUser.setPassword("password");
-        newUser.setUsername("username");
-        userDao.add(newUser);
-        User user = userDao.get(newUser.getUsername());
-        assert userDao.getCount()==1 : "add failed";
-        assert user.getUsername().equals(newUser.getUsername());
-        assert user.getPassword().equals(newUser.getPassword());
+
+        //Act
+        userDao.add(newUser1);
+        userDao.add(newUser2);
+
+        //Assert
+        assert userDao.getCount()==2 : "add failed";
+
+        //Act
+        User user = userDao.get(newUser1.getUsername());
+        User user2 = userDao.get(newUser2.getUsername());
+
+        //Assert
+        assert user.getUsername().equals(newUser1.getUsername()) : "get failed";
+        assert user.getPassword().equals(newUser1.getPassword()) : "get failed";
+        assert user2.getUsername().equals(newUser2.getUsername()) : "get failed";
+        assert user2.getPassword().equals(newUser2.getPassword()) : "get failed";
     }
     @Test
     public void count()  throws ClassNotFoundException, SQLException {
@@ -45,4 +61,5 @@ public class UserDaoTest {
         assert userDao.getCount()==3 : "add failed";
 
     }
+
 }
