db795724c462a0048799a77e17b59f18a65661e9
Jinhyuk <kobin1970@gmail.com>
2025-01-03 14:17:51 +0100

    add transctioHandler

---
+++ src/main/java/com/example/service/TransactionHandler.java
@@ -0,0 +1,44 @@
+package com.example.service;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TransactionHandler implements InvocationHandler {
+
+
+    PlatformTransactionManager transactionManager;
+
+
+
+    Object target;
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+    public PlatformTransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        invokeInTransaction(method, args);
+    }
+    public Object invokeInTransaction(Method method, Object[] args) throws Throwable {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object ret = method.invoke(target,args);
+            transactionManager.commit(status);
+            return ret;
+        } catch (InvocationTargetException e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
